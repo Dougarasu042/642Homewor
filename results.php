b4bbab0a34978e57513c2a779aa95b8c51e0c879
Douglas Hurtado Aragon <dhurtadoaragon@mail.sfsu.edu>
2021-11-30 18:27:58 -0800

    trying post

--- results.php
+++ results.php
@@ -1,8 +1,23 @@
-<html>
- <body>
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>CodePen - Registration Form (Bootstrap 5 Validation)</title>
+  <link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css'><link rel="stylesheet" href="./style.css">
 
- Hello <?php echo $_POST["Lastname"]; ?>!<br>
- Your mail is <?php echo $_POST["FirstName"]; ?>.
+</head>
+<body>
+<!-- partial:index.partial.html -->
 
- </body>
- </html>
+
+
+<div class="form-body">
+        Hello <?php echo $_POST["Lastname"]; ?>!<br>
+ Your mail is <?php echo $_POST["FirstName"]; ?>. 
+    </div>
+<!-- partial -->
+
+</body>
+</html>
+
+
