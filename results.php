bb40451651bae950c8614223aa75065767f84f25
Douglas Hurtado Aragon <dhurtadoaragon@mail.sfsu.edu>
2021-11-30 19:36:22 -0800

    Commiting the latestupdates to the values

--- results.php
+++ results.php
@@ -12,12 +12,80 @@
 
 
 <div class="form-body">
-        Hello <?php echo $_POST["Lastname"]; ?>!<br>
- Your mail is <?php echo $_POST["FirstName"]; ?>. 
+        <div class="row">
+            <div class="form-holder">  
+                <div class="form-content">
+                    <div class="form-items" class="form-inline">
+                        <h3>CSC 642 848 Fall 2021 Individual Assignment
+                        </h3>
+                        <h3>Hurtado Aragon Douglas
+                        </h3>
+                        <p>Data survey form.</p>
+                        <form class="requires-validation" action="results.php" method="post" novalidate>
+
+                            <div class="col-md-12 mt-3">
+                            Last name <?php echo $_POST["Lastname"]; ?><br>      
+                            </div>
+                            <div class="col-md-12">
+                            First name <?php echo $_POST["FirstName"]; ?><br>      
+                             </div>
+
+                             <div class="col-md-12">
+                             First name <?php echo $_POST["title"]; ?><br>
+                             
+                           </div>
+
+                           <div class="col-md-12">
+                           Height <?php echo $_POST["Height"]; ?><br>     
+                         </div>
+
+                         <div class="col-md-12">
+                         Phone Number <?php echo $_POST["Phone"]; ?><br>       
+                         </div>
+
+                         <div class="col-md-12">
+                         Street address <?php echo $_POST["Street"]; ?><br>
+      
+                         </div>
+                         <div class="col-md-12">
+                         City <?php echo $_POST["City"]; ?><br>     
+                         </div>
+
+                         <div class="col-md-12">
+                         State <?php echo $_POST["State"]; ?><br>      
+                         </div>
+
+                         <div class="col-md-12">
+                         zip <?php echo $_POST["zip"]; ?><br>
+     
+                         </div>
+                      
+
+                            <div class="col-md-12">
+                            Email: <?php echo $_POST["email"]; ?><br>
+
+                            </div>
+
+                            <div class="col-md-12">
+                            Budget <?php echo $_POST["budget"]; ?><br>
+
+                            </div>
+
+                              
+                  
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 <!-- partial -->
+  <script  src="./script.js">
+</script>
 
 </body>
 </html>
 
 
+        Hello <?php echo $_POST["Lastname"]; ?>!<br>
+ Your mail is <?php echo $_POST["FirstName"]; ?>. 
